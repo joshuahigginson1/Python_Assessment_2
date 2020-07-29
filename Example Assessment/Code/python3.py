b2005fff70cf2f8e6e5a004bccfa3c5cfee3765d
Josh Higginson <joshuahigginson1@gmail.com>
2020-07-29 18:36:12 +0100

    3 q's to go, 30 mins left on 2 hour timer.

--- Example Assessment/Code/python3.py
+++ Example Assessment/Code/python3.py
@@ -1,219 +1,238 @@
-    	# INSTRUCTIONS
-
-	# In case it is not clear, the Question appears first, then examples, then any hints and finally the function that you need to complete appears underneath:
-
-	# <QUESTION>
-
-	# <EXAMPLES>
-
-	# <HINT>
-
-	# You are NOT allowed access to the internet for this assessment, instead you should use the DOCUMENTATION that comes bundled with your Python installation.  You should already be comfortable accessing this documentation, but to summarise:
-
-	# Access Python from you CLI
-
-	# Type help() or for example help(str)
-	
-	
-	# <QUESTION 1>
-
-	# Write a function which takes a string of numbers, separated by commas, and returns all the odd numbers as a string, separated by commas.
-	
-	# If there are no odd numbers then the function should return "null".
-
-	# Inputs will always be given in ascending order, eg "3,4,5". Outputs should also be given in ascending order.
-
-	# <EXAMPLES>
-
-	# one("1,2,3,4,5") → "1,3,5"
-	# one("2,4,6,8") → "null"
-	# one("23,48,52,57") → "23,57"
-
-	# <HINT>
-	# What were the name of the functions we used to get a list from a string, and get a string from a list?
-	# Use help(str) on your CLI for more information.
+# <QUESTION 1>
 
 def one(input):
-	return "null"
+    num_list = []
+    output_list = []
+    input_list = input
 
-	# <QUESTION 2>
+    input_list = input.split(',')
 
-    # Given an Array of Strings, remove any duplicate Strings that occur, then return the Array.
-    # Do not ignore case.
+    for number in input_list:
 
-    # <EXAMPLES>
+        number_int = int(number)
 
-	# two(["Dog"]) → ["Dog"]
-	# two(["Dog","Cat"]) → ["Dog","Cat"]
-	# two(["Dog","Dog","Cat"]) → ["Dog","Cat"]
-    # two(["Dog","DoG","Cat"]) → ["Dog","DoG","Cat"]    
+        if number_int % 2 != 0:
+            num_list.append(int(number_int))
+        else:
+            continue
 
-	# <HINT>
-	# How did we check if an entry was NOT IN a list?
+    output_string = str(num_list)
+    output_string = output_string.strip('[]')
+    output_string = output_string.replace(" ", "")
+
+    if output_string == '':
+        output_string = 'null'
+
+    return output_string
+
+
+# <QUESTION 2>
 
 def two(input):
-	return []
+    full_list = input
+    output_list = []
 
-	# <QUESTION 3>
+    for words in full_list:
+        if words not in output_list:
+            output_list.append(words)
+        else:
+            continue
 
-    # Write a function which, given a string input, returns a string which contains only the characters with odd indexes.
+    return output_list
 
-	# <EXAMPLES>
 
-	# three("Hello") → "el"
-	# three("hi") → "i"
-	# three("0H1e2l3l4o5w6o7r8l9d") → "Helloworld"
+# <QUESTION 3>
 
-	# <HINT>
-	# How do we choose only the odd numbers when using range()?
 
 def three(input):
-	return ""
+    full_list = input
+    output_list = ''
 
-	# <QUESTION 4>
+    for letters in full_list[1::2]:
+        output_list += letters
 
-    # given a string - return the number of times "am" appears in the String
-	# ignoring case -
-	# BUT ONLY WHEN the word "am" appears without being followed or preceded by
-	# other letters
-	    
-    # <EXAMPLES>
+    return output_list
 
-	# four("Am I in Amsterdam") → 1
-	# four("I am in Amsterdam am I?") → 2
-	# four("I have been in Amsterdam") → 0
 
-	# <HINT>
-	# What function do we use to get a list from a string? Use help(str) on your CLI for more information.
+# <QUESTION 4>
+
 
 def four(arg1):
-	return 0
+    input = str(f" {arg1.lower()} ")
+    check = " am "
+    return input.count(check)
 
-	# <QUESTION 5>
 
-    # Write a function which checks the validity of a credit card number.
-	# The function will take a string and should return a boolean, True if the card is valid, or False if it is not.
+# <QUESTION 5>
 
-	# A credit card has a valid number if it satisfies the following criteria.
-	# - it must start with a 4, 5 or 6.
-	# - it must contain exactly 16 digits.
-	# - each digit must be 0-9 inclusive.
-	# - it may contain hyphens ("-"), to separate groups of 4 digits only, but it cannot contain any other characters.
-	# - it must not have 4 or more consecutive repeated digits.
+# Write a function which checks the validity of a credit card number.
+# The function will take a string and should return a boolean, True if the card is valid, or False if it is not.
 
-	# <EXAMPLES>
+# A credit card has a valid number if it satisfies the following criteria.
+# - it must start with a 4, 5 or 6.
+# - it must contain exactly 16 digits.
+# - each digit must be 0-9 inclusive.
+# - it may contain hyphens ("-"), to separate groups of 4 digits only, but it cannot contain any other characters.
+# - it must not have 4 or more consecutive repeated digits.
 
-	# five(0123-4567-8901-2345) → False
-	# five(401234567890123) → False
-	# five(4012 3456 7890 1234) → False
-	# five(4444-0123-4567-8901) → False
-	# five(4012-3456-7890-1234) → True
-	# five(4012345678901234) → True
+# <EXAMPLES>
 
-	# <HINT>
-	# How did we check if an entry was NOT IN a list?
-	# Think about maybe nesting 'if statements' and/or 'for loops'.
+# five(0123-4567-8901-2345) → False
+# five(401234567890123) → False
+# five(4012 3456 7890 1234) → False
+# five(4444-0123-4567-8901) → False
+# five(4012-3456-7890-1234) → True
+# five(4012345678901234) → True
+
+# <HINT>
+# How did we check if an entry was NOT IN a list?
+# Think about maybe nesting 'if statements' and/or 'for loops'.
 
 def five(input):
-	return False
+valid_chars_str = ['0','1','2','3','4','5','6','7','8','9','-']
+nums = [1,2,3,4,5,6,7,8,9]
+valid_start_nums = [4,5,6]
 
-	# <QUESTION 6>
+output = False
 
-    # Given an email address person@company.com, and a parameter "person" or "company",
-	# write a function which returns the corresponding piece of information.
-	# Your function should ignore case.
+credit_list = []
 
-	# <EXAMPLES>
+    for numbers in input:
+		if numbers in valid_chars_str:
+			credit_list.append(numbers)
 
-	# six("john@google.com", "person") → john
-	# six("jane@Microsoft.com", "company") → microsoft
-	# six("Dave@amazon.com", "person") → dave
+			if int(credit_list[0]) in valid_start_nums:
 
-	# <HINT>
-	# How can you split the email address up? Use help(str) on your CLI for more information.
+
+
+
+			else:
+				output = False
+
+
+		else:
+			output = False
+
+
+
+
+
+
+
+    take string
+
+    must start with 4, 5, or 6
+
+    len(obj) == 16
+
+
+if no hypens, thats great. if hypens, check in groups of four.
+
+    must not have 4 or more consecutive digits.
+
+    output bool
+
+
+    return False"""
+
+
+# <QUESTION 6>
+
 
 def six(email, parameter):
-	return ""
+    name_url_split_list = email.split("@")
+    name = name_url_split_list[0].lower()
+    comp_url = name_url_split_list[1]
+    comp_url_split_list = comp_url.split(".")
+    comp = comp_url_split_list[0].lower()
 
-	# <QUESTION 7>
+    if parameter == 'person':
+        output = name
+    if parameter == 'company':
+        output = comp
 
-    # Given a string, return the length of the largest "block" in the string.
-	# A block is a run of adjacent chars that are the same, do not ignore case.
-	
-    # <EXAMPLES>
-	
-	# seven("hoopplla") → 2
-	# seven("abbCCCddDDDeeEEE") → 3
-	# seven("") → 0
+    return output
 
-	# <HINT>
-	# Try using nested for loops and keep track of the longest block.
+
+# <QUESTION 7>
+
+# Given a string, return the length of the largest "block" in the string.
+# A block is a run of adjacent chars that are the same, do not ignore case.
+
+# <EXAMPLES>
+
+# seven("hoopplla") → 2
+# seven("abbCCCddDDDeeEEE") → 3
+# seven("") → 0
+
+# <HINT>
+# Try using nested for loops and keep track of the longest block.
+
+"""def seven(input):
     
-def seven(input):
-	return -1
+    count = 0
+    
+    for letters in input:
+        if 
+    
+    
+    return """
+"""
 
-	# <QUESTION 8>
+# <QUESTION 8>
 
-    # There is a mathematical function which is defined in the following way:
-
-	# eight(n)=0 if n=0
-	# eight(n)=1 if n=1
-	# eight(n)=eight(n-1)+eight(n-2) if n>1
-
-	# eg eight(2) = eight(1) + eight(0) = 1 + 0 = 1, so eight(2) = 1
-
-	# Write a Python function which outputs the result of the mathematical function, given an input n.
-
-	# <EXAMPLES>
-
-	# eight(3) → 2
-	# eight(8) → 21
-	# eight(0) → 0
-	# eight(1) → 1
-
-	# <HINT>
-	# You can have nested functions in Python.
 
 def eight(n):
-	return 0
+    if n == 0:
+        output_eight = 0
+    elif n == 1:
+        output_eight = 1
+    elif n > 1:
+        output_eight = eight(n - 1) + eight(n - 2)
 
-	# <QUESTION 9>
+    return output_eight
 
-    # Write a function which solves the following puzzle.
-	
-	# We count 35 heads and 94 legs among the chickens and rabbits in a farm. How many rabbits and how many chickens do we have?
-	# The values for 'heads' and 'legs' will be inputs and can be different from 35 and 94.
-	# The output should be of the form (chickens,rabbits).
-	# If there are no solutions to the puzzle, return "no solutions"
 
-	# <EXAMPLES>
+# <QUESTION 9>
 
-	# nine(35, 94) → (23,12)
-	# nine(2, 6) → (1,1)
-	# nine(12,63) → "no soltuions"
+# Write a function which solves the following puzzle.
 
-	# <HINT>
-	# How many legs/heads does a chicken have?
-	# How many legs/heads does a rabbit have?
-	# If we have the right number of legs, do we have the right number of heads?
+# We count 35 heads and 94 legs among the chickens and rabbits in a farm. How many rabbits and how many chickens do we have?
+# The values for 'heads' and 'legs' will be inputs and can be different from 35 and 94.
+# The output should be of the form (chickens,rabbits).
+# If there are no solutions to the puzzle, return "no solutions"
 
-def nine(heads,legs):
-	return "no solutions"
+# <EXAMPLES>
 
-	# <QUESTION 10>
+# nine(35, 94) → (23,12)
+# nine(2, 6) → (1,1)
+# nine(12,63) → "no soltuions"
 
-    # Write a function that accepts a comma separated sequence of lower case words as input and returns the words in a comma-separated
-	# sequence after sorting them alphabetically.
+# <HINT>
+# How many legs/heads does a chicken have? chicken has 2 legs and 1 head.
+# How many legs/heads does a rabbit have? rabbit has 4 legs and 1 head.
+# If we have the right number of legs, do we have the right number of heads?
 
-	# <EXAMPLES>
 
-	# ten('bag,car,dog') → "bag,car,dog"
-	# ten('dog,car,bag') → "bag,car,dog"
-	# ten('car,bark,bag,dog') → "bag,bark,car,dog"
+'''
+"def nine(heads, legs):
+    return "no solutions"
+'''
 
-	# <HINT>
-	# What was the function which we used to sort a list in ascending numeric order? It works on strings too.
-	# Use help(list) on your CLI for more information.
+
+# <QUESTION 10>
+
 
 def ten(input):
-	return ""
+    list = input.split(",")
+    output_list = sorted(list)
+    output_str = str()
+
+    for word in output_list:
+        output_str = output_str + str(f"{word},")
+
+    output_str = output_str.replace(" ", "")
+    output_str = output_str.strip("[]")
+    output_str = output_str[:-1]
+
+    return output_str
