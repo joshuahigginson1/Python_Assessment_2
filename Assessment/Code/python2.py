3449004512930a1ff0033e7def3970d65852f87b
Josh Higginson <joshuahigginson1@gmail.com>
2020-07-30 11:38:46 +0100

    quick reformat

--- Assessment/Code/python2.py
+++ Assessment/Code/python2.py
@@ -128,15 +128,6 @@ def seven(a, b, c):
 
 # <QUESTION 8>
 
-# Given a string and an integer, n, return a string that removes n letters from the 'middle' of the string.
-
-# The string length will be at least n, and be odd when the length of the input is odd, so there will always be a 'middle'.
-
-# <EXAMPLES>
-
-# eight("Hello", 3) → "Ho"
-# eight("Chocolate", 3) → "Choate"
-# eight("Chocolate", 1) → "Choclate"
 
 def eight(input, a):
 
